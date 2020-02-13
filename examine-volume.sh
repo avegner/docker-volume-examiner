7a9306d0302f07e41f7c6adc29db5f1726a037e0
Aleksei Vegner <alexey.vegner@yandex.ru>
2020-02-13 18:20:29 +0300

    Refactor all checks

--- examine-volume.sh
+++ examine-volume.sh
@@ -2,13 +2,35 @@
 IMAGE=volume-examiner:latest
 VOLUME=${1}
 
-if ! docker inspect ${IMAGE} >/dev/null 2>&1 || ! make -f check-mod-time >/dev/null 2>&1; then
-  docker build -t ${IMAGE} -f volume-examiner .
+suppress_out()
+{
+  ${@} >/dev/null 2>&1
+}
+
+volume_exist()
+{
+  suppress_out docker volume inspect ${1}
+}
+
+image_exist()
+{
+  suppress_out docker image inspect ${1}
+}
+
+image_fresh()
+{
+  suppress_out make -f check-image-freshness
+}
+
+if ! image_exist ${IMAGE} || ! image_fresh; then
+  echo 'generating examiner image...'
+  docker build -t ${IMAGE} .
 fi
 
-if ! docker inspect ${VOLUME} 2>/dev/null | grep Mountpoint >/dev/null 2>&1; then
+if ! volume_exist ${VOLUME}; then
   echo "volume \"${VOLUME}\" doesn't exist"
   exit 1
 fi
 
+echo 'running examiner container...'
 docker run -it --rm -v ${VOLUME}:/ro-volume:ro ${IMAGE}
